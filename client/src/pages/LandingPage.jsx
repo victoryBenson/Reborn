b3ace06db7b0fc5e83e359738caff72a9fd6d11d
Kennytech01 <Kennysonvictory@gmail.com>
2024-03-16 17:20:39 +0100

    update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { GetProducts } from "../component/GetProducts";
 import { Testimonial } from "../component/Testimonial";
+import { Hero } from "../component/Hero";
 
 export const LandingPage = () => {
   return (
     <>
       {/* <DiscoverMore /> */}
       <GetProducts />
-      <Testimonial />
+      {/* <Testimonial /> */}
+      <Hero/>
     </>
   );
 };
